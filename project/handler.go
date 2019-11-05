16e1577c8926095675bb93403c9c47bb58c97257
Abigail Beets <abigail.beets@workiva.com>
2019-11-05 12:08:29 -0700

    add database unit test

--- project/handler.go
+++ project/handler.go
@@ -16,13 +16,6 @@ type Projects struct {
 }
 
 func (handler *ProjectHandler) GetAllProjectsHandler(w http.ResponseWriter, r *http.Request) {
-	// supposing this was part of the request body, we'd add a struct tag to the
-	// request body struct containing a constraint on the accountID and ensure the
-	// accountID is safe to use in our query and reject if
-	// it does not meet requirements around length or another criteria
-	// such as being only alphanumeric characters. For this example, just
-	// going to check if it's 32 characters long and not make it part of
-	// the request body.
 	accountID := os.Getenv("ACCOUNT_ID")
 	if len(accountID) < 32 {
 		helpers.RespondWithStatus(w, http.StatusForbidden)
---
+++ project/repository_test.go
@@ -0,0 +1,57 @@
+package project_test
+
+import (
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "example-db/project"
+)
+
+var _ = Describe("ProjectRepository", func() {
+	Context("#GetAllProjectsForAccount", func() {
+		It("should return a list of projects for given accountID on success", func() {
+			db, mockDB, _ := sqlmock.New()
+			defer db.Close()
+
+			testAccountID := "accountid"
+			expectedProject := Project{
+				ProjectID:  1,
+				AccountID:  testAccountID,
+				Name:       "TestProject",
+				Type:       "Parsing",
+				CreatedBy:  "Dennis Reynolds",
+				CreatedOn:  "October 12, 2019",
+				ModifiedBy: "Dennis Reynolds",
+				ModifiedOn: "October 13, 2019",
+			}
+
+			columns := []string{"ProjectID", "AccountID", "Name",
+				"Type", "CreatedBy", "CreatedDate",
+				"ModifiedBy", "ModifiedDate"}
+
+			mockDB.ExpectQuery("SELECT (.+) FROM DataModeler.Project").
+				WillReturnRows(sqlmock.NewRows(columns).AddRow(expectedProject.ProjectID, testAccountID, expectedProject.Name, expectedProject.Type, expectedProject.CreatedBy, expectedProject.CreatedOn, expectedProject.ModifiedBy, expectedProject.ModifiedOn))
+
+			projectRepository := ProjectRepository{DB: db}
+			projects, err := projectRepository.GetAllProjectsForAccount(testAccountID)
+			Expect(err).ToNot(HaveOccurred())
+
+			if err := mockDB.ExpectationsWereMet(); err != nil {
+				Fail(err.Error())
+			}
+
+			Expect(len(projects)).To(Equal(1))
+
+			project := projects[0]
+			Expect(project.ProjectID).To(Equal(expectedProject.ProjectID))
+			Expect(project.AccountID).To(Equal(expectedProject.AccountID))
+			Expect(project.Name).To(Equal(expectedProject.Name))
+			Expect(project.Type).To(Equal(expectedProject.Type))
+			Expect(project.CreatedBy).To(Equal(expectedProject.CreatedBy))
+			Expect(project.CreatedOn).To(Equal(expectedProject.CreatedOn))
+			Expect(project.ModifiedBy).To(Equal(expectedProject.ModifiedBy))
+			Expect(project.ModifiedOn).To(Equal(expectedProject.ModifiedOn))
+		})
+	})
+})
